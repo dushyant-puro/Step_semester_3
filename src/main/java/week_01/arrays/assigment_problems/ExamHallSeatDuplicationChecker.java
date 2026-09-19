b2991f1392cc3be65f5a158d4c8919da04c2cfa3
dushyant-puro <137048721+dushyant-puro@users.noreply.github.com>
2026-09-19 11:56:56 +0530

    Add Week 1 assignment: seat duplication checker

---
+++ src/main/java/week_01/arrays/assigment_problems/ExamHallSeatDuplicationChecker.java
@@ -0,0 +1,30 @@
+package week_01.arrays.assigment_problems;
+
+public class ExamHallSeatDuplicationChecker {
+    public static void checkDuplicateSeats(int[] seatNumbers) {
+        boolean found = false;
+        for (int i = 0; i < seatNumbers.length; i++) {
+            boolean appearedEarlier = false;
+            for (int k = 0; k < i; k++) {
+                if (seatNumbers[k] == seatNumbers[i]) {
+                    appearedEarlier = true;
+                    break;
+                }
+            }
+            if (appearedEarlier) continue;
+            for (int j = i + 1; j < seatNumbers.length; j++) {
+                if (seatNumbers[i] == seatNumbers[j]) {
+                    System.out.println("Duplicate Seat Number Found: " + seatNumbers[i]);
+                    found = true;
+                    break;
+                }
+            }
+        }
+        if (!found) System.out.println("No Duplicate Seats Found");
+    }
+
+    public static void main(String[] args) {
+        checkDuplicateSeats(new int[]{101, 102, 103, 102, 105});
+        checkDuplicateSeats(new int[]{101, 102, 103, 104, 105});
+    }
+}
